a0c71070fea67544b609a6d451a64cfdfddb4395
Kidus Tesema <tkidusaklog@gmail.com>
2026-04-15 11:48:11 -0500

    Added Exercise 1B SQL file

---
+++ week-02/Ex1B_first_query.sql
@@ -0,0 +1,10 @@
+/*
+Sakila is a relational database designed to simulate a DVD rental store.
+It contains structured data about films, actors, customers, inventory, rentals, and payments,
+which can be used for practicing SQL queries and data analysis.
+*/
+
+USE sakila;
+select * FROM actor; -- Retrieved 200 records
+
+SELECT * FROM film; -- Retrieved 1000 records
