55d53fadc326f57ba4a6fe491895bf1e620e6719
kidus A Tesema <114831423+kidusaklog@users.noreply.github.com>
2026-05-06 20:48:27 -0500

    Added hello.py to week-05

--- week-05/Hello.py
+++ week-05/Hello.py
@@ -1,1 +1,30 @@
-print('Hello world!')
+print("Hello world!")
+
+
+
+# Create a variable and print it
+message = "Hello world!"
+print(message)
+
+# It prints twice because there are two print statements:
+# one directly printing the string and another printing the variable.
+
+
+# Displaying dollars and cents
+dollars = 3
+cents = 0.50
+
+print(dollars + cents)
+
+# Observation:
+# Python adds the numeric values together and displays 3.5
+
+
+cents = cents + 0.25
+print(dollars + cents)
+
+
+d_str = "3 dollars"
+c_str = "50 cents"
+
+print(d_str + " " + c_str)
